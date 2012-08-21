733ffb8e53cfc66d32f4f06e7769f5bafa085139
Maxim Andryunin <maxim.andryunin@gmail.com>
2012-08-21 18:06:04 +0700

    Simple counter test added.

--- allpoll/tests.py
+++ allpoll/tests.py
@@ -1,16 +1,21 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
+from allpoll.models import Poll, Choice, Vote
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+
+class CounterTest(TestCase):
+    fixtures = ['test_data.json']
+
+    def testCounter(self):
+        poll = Poll.objects.order_by('pk')[0]
+        choice = poll.choice_set.all()
+
+        choice[0].vote()
+        choice[1].vote(10)
+        choice[1].vote(-5)
+
+        poll = Poll.objects.order_by('pk')[0]
+
+        self.assertEqual(choice[0].count, 1)
+        self.assertEqual(choice[1].count, 5)
+        self.assertEqual(poll.count, 6)
