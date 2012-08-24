a3c8dca72ec15a2a4af87c23c5c5a21b23b6f2d1
Maxim Andryunin <maxim.andryunin@gmail.com>
2012-08-24 22:45:14 +0700

    Setup script fixed, version updated.

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
 setup(name='django-polls',
-      version='0.1',
-      description='Polling application for Django',
+      version='0.9',
+      description='Simple polling application for Django',
       author='Maxim Andryunin',
       author_email='maxim.andryunin@gmail.com',
-      packages=['polls'],
+      packages=['allpoll'],
      )
