24e19fc2e41be9cd45d50d10d93824b575c824c0
Maxim Andryunin <maxim.andryunin@gmail.com>
2012-08-24 21:06:33 +0700

    Bug in list view fixed.

--- allpoll/settings.py
+++ allpoll/settings.py
@@ -2,4 +2,4 @@ from django.conf import settings
 
 IP_LIMIT = getattr(settings, 'POLLS_IP_LIMIT', 50)
 
-PAGINATE_BY = getattr(settings, 'POLLS_IP_LIMIT', 1)
+PAGINATE_BY = getattr(settings, 'POLLS_IP_LIMIT', 10)
